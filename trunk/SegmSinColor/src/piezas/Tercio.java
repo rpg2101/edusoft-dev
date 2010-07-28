65d251361e7276c4bfe079d9701b0ba373878a49
hectorrattis <hectorrattis@e45fdbd4-8ce8-1581-495e-e82c5ef16aaf>
2010-07-28 18:33:11 +0000

    Segundo ejercicio de segmentos. Dado una pieza se deben completar el entero usando piezas de distintos enteros (medios, cuartos y octavos) hasta lograr formar todas las posibles combinaciones.

---
+++ trunk/SegmSinColor/src/piezas/Tercio.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package piezas;
+
+import java.awt.Color;
+import java.awt.Rectangle;
+import segmsincolor.Lienzo;
+
+/**
+ *
+ * @author curio
+ */
+public class Tercio extends Pieza {
+
+    public Tercio(int x0, int y0, int ainicial, Lienzo l) {
+        super(x0, y0, ainicial, l);
+        setAngfinal(120);
+        setColor(Color.ORANGE);
+    }
+
+    @Override
+    public Rectangle segArrastre() {
+        Rectangle tmp = null;
+        int tam_W = getTamelipse() * 1 / 4;
+        int tam_H = getTamelipse() * 1 / 3;
+        switch (this.getAnginicial()) {
+            case 90:
+                tmp = new Rectangle(getX() + 20, getY() + 40, tam_W + 25,
+                        tam_H + 5);
+                break;
+            case 210:
+                tmp = new Rectangle(getX() + 55, getY() + 130, tam_H + 25,
+                        tam_W + 5);
+                break;
+            case 330:
+                tmp = new Rectangle(getX() + 105, getY() + 40, tam_W + 25,
+                        tam_H + 5);
+                break;
+        }
+        return tmp;
+    }
+}
