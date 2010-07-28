2673a3a0cf2bcc7c547a7ba9187017b592f03cbe
hectorrattis <hectorrattis@e45fdbd4-8ce8-1581-495e-e82c5ef16aaf>
2010-07-28 22:50:46 +0000

    Animacion primer paso

--- trunk/Edusoft/src/edusoft/ventanas/Lienzo.java
+++ trunk/Edusoft/src/edusoft/ventanas/Lienzo.java
@@ -21,7 +21,7 @@ public class Lienzo extends Canvas {
 
     private Vector<Vector> elemento;
     private Image texto1;
-    private Image basepapel;
+    private Image basepapel, puntapapel;
     private boolean modificaranchod, modificaraltoup;
     private boolean modificaranchoi, modificaraltodown;
     private boolean outi, outup, outdown, outd;
@@ -30,8 +30,15 @@ public class Lienzo extends Canvas {
     @Override
     public boolean mouseMove(Event evt, int x, int y) {
         if (modificaranchod) {
+
             this.getElem().elementAt(0).set(3, x - 450);
             this.getElem().elementAt(1).set(3, x - 450);
+            this.getElem().elementAt(4).set(3,( (800 - x) / 2)+10);
+            this.getElem().elementAt(4).set(1, x);
+            this.getElem().elementAt(6).set(3, (800 - x) / 2);
+            this.getElem().elementAt(6).set(1, x + 10);
+            this.getElem().elementAt(5).set(1,(x + (800 - x) / 2)  );
+
             if (x >= 100 && x <= 800) {
                 this.outd = false;
                 this.repaint();
@@ -78,8 +85,8 @@ public class Lienzo extends Canvas {
             }
         }
         if (modificaraltodown) {
-            this.getElem().elementAt(1).set(4, y -275);
-            this.getElem().elementAt(2).set(4, y -275);
+            this.getElem().elementAt(1).set(4, y - 275);
+            this.getElem().elementAt(2).set(4, y - 275);
             if (y >= 150 && y <= 400) {
                 this.outdown = false;
                 this.repaint();
@@ -136,6 +143,7 @@ public class Lienzo extends Canvas {
         setBackground(Color.BLACK);
         texto1 = new ImageIcon(getClass().getResource("1.png")).getImage();
         basepapel = new ImageIcon(getClass().getResource("basepapel.png")).getImage();
+        puntapapel = new ImageIcon(getClass().getResource("puntapapel.png")).getImage();
         elemento = new Vector<Vector>();
         // Primer Cuadrante
         Vector<Integer> aux1 = new Vector<Integer>();
@@ -172,6 +180,28 @@ public class Lienzo extends Canvas {
         //Rectangulo auxiliar para interseccion
         rectangulo = new Rectangle(100, 150, 700, 250);
 
+        Vector<Integer> rectdoblez1 = new Vector<Integer>();
+        rectdoblez1.add(1);
+        rectdoblez1.add(800);
+        rectdoblez1.add(150);
+        rectdoblez1.add(0);
+        rectdoblez1.add(250);
+        elemento.add(rectdoblez1);
+        Vector<Integer> rectdoblez2 = new Vector<Integer>();
+        rectdoblez2.add(2);
+        rectdoblez2.add(800);
+        rectdoblez2.add(170);
+        rectdoblez2.add(0);
+        rectdoblez2.add(250);
+        
+        Vector<Integer> rectdoblez3 = new Vector<Integer>();
+        rectdoblez3.add(3);
+        rectdoblez3.add(780);
+        rectdoblez3.add(150);
+        rectdoblez3.add(20);
+        rectdoblez3.add(20);
+        elemento.add(rectdoblez3);
+        elemento.add(rectdoblez2);
         this.setBackground(Color.LIGHT_GRAY);
         this.modificaranchod = false;
         this.modificaranchoi = false;
@@ -186,31 +216,55 @@ public class Lienzo extends Canvas {
     @Override
     public void paint(Graphics g) {
 
-        g.setColor(Color.LIGHT_GRAY);
+        g.setColor(Color.WHITE);
         g.fillRect(0, 0, getWidth(), getHeight());
         g.drawImage(texto1, 50, 40, 850, 100, this);
 
         for (int i0 = 0; i0 < getElem().size(); i0++) {
             if ((Integer) getElem().elementAt(i0).get(0) == 0) {
-                g.drawImage(basepapel, (Integer) getElem().elementAt(i0).get(1),
-                        (Integer) getElem().elementAt(i0).get(2),
-                        (Integer) getElem().elementAt(i0).get(3),
-                        (Integer) getElem().elementAt(i0).get(4), this);
-
-            }
-            if ((Integer) getElem().elementAt(i0).get(0) == 1) {
-                g.setColor(Color.RED);
+//                g.drawImage(basepapel, (Integer) getElem().elementAt(i0).get(1),
+//                        (Integer) getElem().elementAt(i0).get(2),
+//                        (Integer) getElem().elementAt(i0).get(3),
+//                        (Integer) getElem().elementAt(i0).get(4), this);
+                
+                g.setColor(Color.LIGHT_GRAY);
                 g.fillRect((Integer) getElem().elementAt(i0).get(1),
                         (Integer) getElem().elementAt(i0).get(2),
                         (Integer) getElem().elementAt(i0).get(3),
                         (Integer) getElem().elementAt(i0).get(4));
+
+            }
+            if ((Integer) getElem().elementAt(i0).get(0) == 1) {
+                g.setColor(Color.LIGHT_GRAY);
+                g.fillRect((Integer) getElem().elementAt(i0).get(1),
+                        (Integer) getElem().elementAt(i0).get(2),
+                        (Integer) getElem().elementAt(i0).get(3),
+                        (Integer) getElem().elementAt(i0).get(4));
+            }
+            if ((Integer) getElem().elementAt(i0).get(0) == 2) {
+                g.setColor(Color.GRAY);
+                g.fillRect((Integer) getElem().elementAt(i0).get(1),
+                        (Integer) getElem().elementAt(i0).get(2),
+                        (Integer) getElem().elementAt(i0).get(3),
+                        (Integer) getElem().elementAt(i0).get(4));
+          }
+            if ((Integer) getElem().elementAt(i0).get(0) == 3) {
+                g.setColor(Color.LIGHT_GRAY);
+                g.fillArc((Integer) getElem().elementAt(i0).get(1),
+                        (Integer) getElem().elementAt(i0).get(2),
+                        (Integer) getElem().elementAt(i0).get(3),
+                        (Integer) getElem().elementAt(i0).get(4),-90,180);
+                g.setColor(Color.BLACK);
+                g.drawArc((Integer) getElem().elementAt(i0).get(1),
+                        (Integer) getElem().elementAt(i0).get(2),
+                        (Integer) getElem().elementAt(i0).get(3),
+                        (Integer) getElem().elementAt(i0).get(4),-90,180);
             }
         }
 
 
         g.setColor(Color.BLUE);
         g.setColor(Color.GREEN);
-        g.drawRect(rectangulo.x, rectangulo.y, rectangulo.width, rectangulo.height);
 
 
 //        FontMetrics fm = g.getFontMetrics();
