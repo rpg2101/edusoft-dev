b639e3321fac9a2aa0aa38bc0fdca92cdc8863d5
rpg2101 <rpg2101@e45fdbd4-8ce8-1581-495e-e82c5ef16aaf>
2010-07-29 02:39:44 +0000

    efecto papel doblado almost done...

--- trunk/Edusoft/src/edusoft/ventanas/Lienzo.java
+++ trunk/Edusoft/src/edusoft/ventanas/Lienzo.java
@@ -21,7 +21,7 @@ public class Lienzo extends Canvas {
 
     private Vector<Vector> elemento;
     private Image texto1;
-    private Image basepapel, puntapapel;
+    private Image pliegue,solapa, basepapel0, basepapel1, basepapel2, basepapel3, puntapapel;
     private boolean modificaranchod, modificaraltoup;
     private boolean modificaranchoi, modificaraltodown;
     private boolean outi, outup, outdown, outd;
@@ -35,10 +35,10 @@ public class Lienzo extends Canvas {
 
             this.getElem().elementAt(0).set(3, x - 450);
             this.getElem().elementAt(1).set(3, x - 450);
-            this.getElem().elementAt(4).set(3, ((800 - x) / 2) + 10);
+            this.getElem().elementAt(4).set(3, (800 - x) / 2);
             this.getElem().elementAt(4).set(1, x);
             this.getElem().elementAt(6).set(3, (800 - x) / 2);
-            this.getElem().elementAt(6).set(1, x + 10);
+            this.getElem().elementAt(6).set(1, x);
             this.getElem().elementAt(5).set(1, (x + (800 - x) / 2));
 
             if (x >= 100 && x <= 800) {
@@ -144,10 +144,14 @@ public class Lienzo extends Canvas {
     public Lienzo() {
         setBackground(Color.BLACK);
         texto1 = new ImageIcon(getClass().getResource("1.png")).getImage();
-        basepapel = new ImageIcon(getClass().getResource("basepapel.png")).getImage();
-        puntapapel = new ImageIcon(getClass().getResource("puntapapel.png")).getImage();
+        basepapel0 = new ImageIcon(getClass().getResource("basepapel0.png")).getImage();
+        basepapel1 = new ImageIcon(getClass().getResource("basepapel1.png")).getImage();
+        basepapel2 = new ImageIcon(getClass().getResource("basepapel2.png")).getImage();
+        basepapel3 = new ImageIcon(getClass().getResource("basepapel3.png")).getImage();
+        solapa = new ImageIcon(getClass().getResource("solapa.png")).getImage();
+        pliegue = new ImageIcon(getClass().getResource("pliegue.png")).getImage();
         elemento = new Vector<Vector>();
-        // Primer Cuadrante
+// Primer Cuadrante
         Vector<Integer> aux1 = new Vector<Integer>();
         aux1.add(0);
         aux1.add(450);
@@ -155,7 +159,7 @@ public class Lienzo extends Canvas {
         aux1.add(350);
         aux1.add(125);
         elemento.add(aux1);
-        // Segundo Cuadrante
+// Segundo Cuadrante
         Vector<Integer> aux2 = new Vector<Integer>();
         aux2.add(0);
         aux2.add(450);
@@ -163,7 +167,7 @@ public class Lienzo extends Canvas {
         aux2.add(350);
         aux2.add(125);
         elemento.add(aux2);
-        // Tercer Cuadrante
+// Tercer Cuadrante
         Vector<Integer> aux3 = new Vector<Integer>();
         aux3.add(0);
         aux3.add(100);
@@ -171,7 +175,7 @@ public class Lienzo extends Canvas {
         aux3.add(350);
         aux3.add(125);
         elemento.add(aux3);
-        // Cuarto Cuadrante
+// Cuarto Cuadrante
         Vector<Integer> aux4 = new Vector<Integer>();
         aux4.add(0);
         aux4.add(100);
@@ -179,32 +183,35 @@ public class Lienzo extends Canvas {
         aux4.add(350);
         aux4.add(125);
         elemento.add(aux4);
-        //Rectangulo auxiliar para interseccion
+//Rectangulo auxiliar para interseccion
         rectangulo = new Rectangle(100, 150, 700, 250);
-
+// Rectangulo inferior del solape
         Vector<Integer> rectdoblez1 = new Vector<Integer>();
         rectdoblez1.add(1);
         rectdoblez1.add(800);
         rectdoblez1.add(150);
         rectdoblez1.add(0);
-        rectdoblez1.add(250);
-        elemento.add(rectdoblez1);
+        rectdoblez1.add(125);
+// Rectangulo Superior del solape
         Vector<Integer> rectdoblez2 = new Vector<Integer>();
         rectdoblez2.add(2);
         rectdoblez2.add(800);
-        rectdoblez2.add(170);
+        rectdoblez2.add(175);
         rectdoblez2.add(0);
         rectdoblez2.add(250);
-
+// Pligue
         Vector<Integer> rectdoblez3 = new Vector<Integer>();
         rectdoblez3.add(3);
         rectdoblez3.add(780);
-        rectdoblez3.add(150);
-        rectdoblez3.add(20);
-        rectdoblez3.add(20);
+        rectdoblez3.add(148);
+        rectdoblez3.add(30);
+        rectdoblez3.add(277);
+
+// Primero el inferior , despues el pliegue , y por ultimo el superior
+        elemento.add(rectdoblez1);
         elemento.add(rectdoblez3);
         elemento.add(rectdoblez2);
-        this.setBackground(Color.LIGHT_GRAY);
+
         this.modificaranchod = false;
         this.modificaranchoi = false;
         this.modificaraltoup = false;
@@ -216,7 +223,11 @@ public class Lienzo extends Canvas {
     }
 
     @Override
-    public synchronized void update(Graphics g) {
+    public void update(Graphics g) {
+        paint(g);
+    }
+
+    public void paint(Graphics g) {
         if (getGB() == null) {
             imag = createImage(1000, 700);
             setGB(imag.getGraphics());
@@ -227,206 +238,233 @@ public class Lienzo extends Canvas {
 
         for (int i0 = 0; i0 < getElem().size(); i0++) {
             if ((Integer) getElem().elementAt(i0).get(0) == 0) {
-//                g.drawImage(basepapel, (Integer) getElem().elementAt(i0).get(1),
-//                        (Integer) getElem().elementAt(i0).get(2),
-//                        (Integer) getElem().elementAt(i0).get(3),
-//                        (Integer) getElem().elementAt(i0).get(4), this);
-
-                getGB().setColor(Color.LIGHT_GRAY);
-                getGB().fillRect((Integer) getElem().elementAt(i0).get(1),
-                        (Integer) getElem().elementAt(i0).get(2),
-                        (Integer) getElem().elementAt(i0).get(3),
-                        (Integer) getElem().elementAt(i0).get(4));
-
+                if (i0 == 0) {
+                    getGB().drawImage(basepapel0, (Integer) getElem().elementAt(i0).get(1),
+                            (Integer) getElem().elementAt(i0).get(2),
+                            (Integer) getElem().elementAt(i0).get(3),
+                            (Integer) getElem().elementAt(i0).get(4), this);
+                }
+                if (i0 == 1) {
+                    getGB().drawImage(basepapel1, (Integer) getElem().elementAt(i0).get(1),
+                            (Integer) getElem().elementAt(i0).get(2),
+                            (Integer) getElem().elementAt(i0).get(3),
+                            (Integer) getElem().elementAt(i0).get(4), this);
+                }
+                if (i0 == 2) {
+                    getGB().drawImage(basepapel2, (Integer) getElem().elementAt(i0).get(1),
+                            (Integer) getElem().elementAt(i0).get(2),
+                            (Integer) getElem().elementAt(i0).get(3),
+                            (Integer) getElem().elementAt(i0).get(4), this);
+                }
+                if (i0 == 3) {
+                    getGB().drawImage(basepapel3, (Integer) getElem().elementAt(i0).get(1),
+                            (Integer) getElem().elementAt(i0).get(2),
+                            (Integer) getElem().elementAt(i0).get(3),
+                            (Integer) getElem().elementAt(i0).get(4), this);
+                }
             }
             if ((Integer) getElem().elementAt(i0).get(0) == 1) {
-                getGB().setColor(Color.LIGHT_GRAY);
-                getGB().fillRect((Integer) getElem().elementAt(i0).get(1),
+                getGB().drawImage(basepapel0, (Integer) getElem().elementAt(i0).get(1),
                         (Integer) getElem().elementAt(i0).get(2),
                         (Integer) getElem().elementAt(i0).get(3),
-                        (Integer) getElem().elementAt(i0).get(4));
+                        (Integer) getElem().elementAt(i0).get(4), this);
+
+
             }
             if ((Integer) getElem().elementAt(i0).get(0) == 2) {
-                getGB().setColor(Color.GRAY);
-                getGB().fillRect((Integer) getElem().elementAt(i0).get(1),
+                getGB().drawImage(solapa, (Integer) getElem().elementAt(i0).get(1),
                         (Integer) getElem().elementAt(i0).get(2),
                         (Integer) getElem().elementAt(i0).get(3),
-                        (Integer) getElem().elementAt(i0).get(4));
+                        (Integer) getElem().elementAt(i0).get(4), this);
+
             }
             if ((Integer) getElem().elementAt(i0).get(0) == 3) {
-                getGB().setColor(Color.LIGHT_GRAY);
-                getGB().fillArc((Integer) getElem().elementAt(i0).get(1),
+                getGB().drawImage(pliegue, (Integer) getElem().elementAt(i0).get(1),
                         (Integer) getElem().elementAt(i0).get(2),
                         (Integer) getElem().elementAt(i0).get(3),
-                        (Integer) getElem().elementAt(i0).get(4), -90, 180);
-                getGB().setColor(Color.BLACK);
-                getGB().drawArc((Integer) getElem().elementAt(i0).get(1),
-                        (Integer) getElem().elementAt(i0).get(2),
-                        (Integer) getElem().elementAt(i0).get(3),
-                        (Integer) getElem().elementAt(i0).get(4), -90, 180);
+                        (Integer) getElem().elementAt(i0).get(4), this);
+
             }
         }
 
-
-        getGB().setColor(Color.BLUE);
-        getGB().setColor(Color.GREEN);
         g.drawImage(imag, 0, 0, null);
+
     }
-    
-    public synchronized void paint(Graphics g){
-    getGB().setColor(Color.WHITE);
-        getGB().fillRect(0, 0, getWidth(), getHeight());
-        getGB().drawImage(texto1, 50, 40, 850, 100, this);
-
-        for (int i0 = 0; i0 < getElem().size(); i0++) {
-            if ((Integer) getElem().elementAt(i0).get(0) == 0) {
-//                g.drawImage(basepapel, (Integer) getElem().elementAt(i0).get(1),
-//                        (Integer) getElem().elementAt(i0).get(2),
-//                        (Integer) getElem().elementAt(i0).get(3),
-//                        (Integer) getElem().elementAt(i0).get(4), this);
-
-                getGB().setColor(Color.LIGHT_GRAY);
-                getGB().fillRect((Integer) getElem().elementAt(i0).get(1),
-                        (Integer) getElem().elementAt(i0).get(2),
-                        (Integer) getElem().elementAt(i0).get(3),
-                        (Integer) getElem().elementAt(i0).get(4));
-
-            }
-            if ((Integer) getElem().elementAt(i0).get(0) == 1) {
-                getGB().setColor(Color.LIGHT_GRAY);
-                getGB().fillRect((Integer) getElem().elementAt(i0).get(1),
-                        (Integer) getElem().elementAt(i0).get(2),
-                        (Integer) getElem().elementAt(i0).get(3),
-                        (Integer) getElem().elementAt(i0).get(4));
-            }
-            if ((Integer) getElem().elementAt(i0).get(0) == 2) {
-                getGB().setColor(Color.GRAY);
-                getGB().fillRect((Integer) getElem().elementAt(i0).get(1),
-                        (Integer) getElem().elementAt(i0).get(2),
-                        (Integer) getElem().elementAt(i0).get(3),
-                        (Integer) getElem().elementAt(i0).get(4));
-            }
-            if ((Integer) getElem().elementAt(i0).get(0) == 3) {
-                getGB().setColor(Color.LIGHT_GRAY);
-                getGB().fillArc((Integer) getElem().elementAt(i0).get(1),
-                        (Integer) getElem().elementAt(i0).get(2),
-                        (Integer) getElem().elementAt(i0).get(3),
-                        (Integer) getElem().elementAt(i0).get(4), -90, 180);
-                getGB().setColor(Color.BLACK);
-                getGB().drawArc((Integer) getElem().elementAt(i0).get(1),
-                        (Integer) getElem().elementAt(i0).get(2),
-                        (Integer) getElem().elementAt(i0).get(3),
-                        (Integer) getElem().elementAt(i0).get(4), -90, 180);
-            }
-        }
-
-
-        getGB().setColor(Color.BLUE);
-        getGB().setColor(Color.GREEN);
-        g.drawImage(imag, 0, 0, null);
-}
 
     @Override
     public boolean mouseDown(Event evt, int x, int y) {
         if (modificaranchod) {
             this.modificaranchod = false;
+
+
             if (outd && x < 100) {
                 this.rectangulo.width = 350;
                 x = 100;
+
+
             }
             if (outd && x > 800) {
                 this.rectangulo.width = 700;
                 x = 800;
+
+
             } else {
                 this.rectangulo.width = x - 100;
+
+
             }
             this.repaint();
+
+
             return true;
+
+
         }
         if (modificaranchoi) {
             this.modificaranchoi = false;
+
+
             if (outi && x < 100) {
                 this.rectangulo.width = 700;
                 x = 100;
+
+
             }
             if (outi && x > 800) {
                 this.rectangulo.width = 350;
+
+
                 this.rectangulo.x = 450;
                 x = 800;
+
+
             } else {
                 this.rectangulo.width = 800 - x;
+
+
                 this.rectangulo.x = x;
+
+
             }
             this.repaint();
+
+
             return true;
+
+
         }
         if (modificaraltodown) {
             this.modificaraltodown = false;
+
+
             if (outdown && y < 150) {
                 this.rectangulo.height = 125;
                 y = 150;
+
+
             }
             if (outdown && y > 400) {
                 this.rectangulo.height = 250;
                 y = 400;
+
+
             } else {
                 this.rectangulo.height = y - 150;
+
+
             }
             this.repaint();
+
+
             return true;
+
+
         }
         if (modificaraltoup) {
             this.modificaraltoup = false;
+
+
             if (outup && y < 150) {
                 this.rectangulo.height = 250;
                 y = 150;
+
+
             }
             if (outup && y > 400) {
                 this.rectangulo.height = 125;
+
+
                 this.rectangulo.y = 275;
                 y = 400;
+
+
             } else {
                 this.rectangulo.height = 400 - y;
+
+
                 this.rectangulo.y = y;
+
+
             }
             this.repaint();
+
+
             return true;
+
+
         } else {
             if (Boolean.valueOf(this.rect(x, y).intersects(rectangulo))) {
                 if ((rectangulo.getMaxX() - 10 < x) && (x < rectangulo.getMaxX() + 10)) {
                     this.modificaranchod = true;
+
+
                 }
             }
             if (Boolean.valueOf(this.rect(x, y).intersects(rectangulo))) {
                 if ((rectangulo.getX() - 10 < x) && (x < rectangulo.getX() + 10)) {
                     this.modificaranchoi = true;
+
+
                 }
             }
             if (Boolean.valueOf(this.rect(x, y).intersects(rectangulo))) {
                 if ((rectangulo.getMaxY() - 10 < y) && (y < rectangulo.getMaxY() + 10)) {
                     this.modificaraltodown = true;
+
+
                 }
             }
             if (Boolean.valueOf(this.rect(x, y).intersects(rectangulo))) {
                 if ((rectangulo.getY() - 10 < y) && (y < rectangulo.getY() + 10)) {
                     this.modificaraltoup = true;
+
+
                 }
             }
             return true;
+
+
         }
     }
 
     private Graphics getGB() {
         return gBuffer;
+
+
     }
 
     private void setGB(Graphics gb) {
         gBuffer = gb;
+
+
     }
 
     public Rectangle rect(int x, int y) {
         Rectangle r = new Rectangle(x - 10, y - 10, 20, 20);
+
+
         return r;
+
     }
 }
 
