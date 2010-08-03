8330abc483e7e3c1499f0aa48192d86e738104a0
hectorrattis <hectorrattis@e45fdbd4-8ce8-1581-495e-e82c5ef16aaf>
2010-08-03 21:37:36 +0000

    Arrastre corregido

--- SegmSinColor/src/piezas/Tercio.java
+++ SegmSinColor/src/piezas/Tercio.java
@@ -10,7 +10,7 @@ import segmsincolor.Lienzo;
 
 /**
  *
- * @author curio
+ * @author Hector Rattis
  */
 public class Tercio extends Pieza {
 
@@ -27,16 +27,16 @@ public class Tercio extends Pieza {
         int tam_H = getTamelipse() * 1 / 3;
         switch (this.getAnginicial()) {
             case 90:
-                tmp = new Rectangle(getX() + 20, getY() + 40, tam_W + 25,
-                        tam_H + 5);
+                tmp = new Rectangle(getX() + 15, getY() + 30, tam_W + 20,
+                        tam_H);
                 break;
             case 210:
-                tmp = new Rectangle(getX() + 55, getY() + 130, tam_H + 25,
-                        tam_W + 5);
+                tmp = new Rectangle(getX() + 35, getY() + 100, tam_H + 30,
+                        tam_W);
                 break;
             case 330:
-                tmp = new Rectangle(getX() + 105, getY() + 40, tam_W + 25,
-                        tam_H + 5);
+                tmp = new Rectangle(getX() + 78, getY() + 30, tam_W + 20,
+                        tam_H);
                 break;
         }
         return tmp;
